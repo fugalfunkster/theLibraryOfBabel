9acd97dbd037ca044a4a7119baba340310ab1049
fugalfunkster <mtthwhsltn@gmail.com>
2016-07-20 22:45:28 -0700

    port to react, break out components, and style
    components: controlPanel
    text.html -> controlPanel
    controlPanel styles: inline and modules, one global
    font data stored in arrays exported through modules in app/
    webpack config and package.json cp from repos/webpack-demo

---
+++ app/components/control-panel/inlineStyles.js
@@ -0,0 +1,69 @@
+import {families} from '../../fontFamilies.js';
+
+exports.header = {
+  fontSize: '9vw',
+  fontWeight: 'normal',
+  fontFamily: families[18],
+  fontFeatureSettings: 'normal',
+  fontKerning: 'normal',
+  fontVariant: 'normal',
+  textAlign: 'center',
+  lineHeight: '2em',
+  color: 'black',
+  margin: '0.33em auto',
+};
+
+exports.byline = {
+  fontSize: '2.5vw',
+  fontWeight: 'normal',
+  fontFamily: families[18],
+  fontFeatureSettings: 'normal',
+  fontKerning: 'normal',
+  fontVariant: 'normal',
+  textAlign: 'center',
+  lineHeight: '0em',
+  color: 'black',
+  marginTop: '0px',
+};
+
+exports.subheader = {
+  fontSize: '5vw',
+  fontWeight: 'normal',
+  fontFamily: families[18],
+  fontFeatureSettings: 'normal',
+  fontKerning: 'normal',
+  fontVariant: 'normal',
+  textAlign: 'center',
+  lineHeight: '1.375em',
+  color: 'black',
+};
+
+exports.paragraphs = {
+  fontSize: '2.1125vw',
+  fontWeight: 'normal',
+  fontFamily: families[18],
+  fontFeatureSettings: 'normal',
+  fontKerning: 'normal',
+  fontVariant: 'normal',
+  textAlign: 'left',
+  lineHeight: '2em',
+  color: 'black',
+};
+
+exports.pullquote = {
+  fontSize: '3vw',
+  fontWeight: 'normal',
+  fontFamily: families[18],
+  fontFeatureSettings: 'normal',
+  fontKerning: 'normal',
+  fontVariant: 'normal',
+  textAlign: 'center',
+  lineHeight: '1.375em',
+  color: 'black',
+};
+
+exports.right = {
+  fontSize: '1.5vw',
+  textAlign: 'right',
+  lineHeight: '1.5em',
+}
---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { webfonts } from './fontFamilies';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ControlPanel from './components/control-panel/controlPanel.js';
+
+const Root = () => (
+  <div>
+    <link rel="stylesheet" type="text/css" href={webfonts}/>
+    <ControlPanel />
+  </div>
+);
+
+var element = document.createElement('div');
+element.id = "app";
+document.body.appendChild(element);
+
+ReactDOM.render(<Root />, document.getElementById('app'));
